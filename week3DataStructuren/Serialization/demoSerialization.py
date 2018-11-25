8a7d338f4d66d07d073dd56fbb1d61fff5158d48
hanbioinformatica <Martijn.vanderBruggen@han.nl>
2018-11-25 20:46:31 +0100

    updateWindows

---
+++ week3DataStructuren/Serialization/demoSerialization.py
@@ -0,0 +1,28 @@
+import pickle
+
+
+def main():
+    naam = "studenten.dat"
+    bepaalBi(naam)
+
+
+
+def bepaalBi(bestandsnaam):
+    meer = 'j'
+    try:
+        input_file = open(bestandsnaam, 'rb')
+        bi1 = pickle.load (input_file)
+        input_file.close()
+    except IOError:
+        bi1 = set()
+    while meer.lower() == 'j':
+        print ("Studenten in Bi1: ",bi1)
+        student = input('Student: ')
+        bi1.add (student)
+        meer = input ("Doorgaan (j/n)")
+    output_file = open(bestandsnaam, 'wb')
+    pickle.dump(bi1, output_file)
+    output_file.close()
+
+main()
+
--- week3DataStructuren/Sets/dierenSet.py
+++ week3DataStructuren/Sets/dierenSet.py
@@ -1,6 +1,8 @@
-boerderij  = set(["Koe","Geit","Varken","Konijn","Konijn"])
+from typing import Set
+
+boerderij: Set[str]  = set(["Koe","Geit","Varken","Konijn","Konijn"])
 dierentuin = set(["Olifant","Giraffe","Zebra", "Konijn","Geit"])
 
-print (boerderij.difference(dierentuin))
+print(boerderij.difference(dierentuin))
 
-print (boerderij.symmetric_difference(dierentuin))
+print(boerderij.symmetric_difference(dierentuin))
