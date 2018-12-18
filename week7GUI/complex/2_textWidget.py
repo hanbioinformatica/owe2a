18104c35b206afa78b3ace09106981b23f3a532d
hanbioinformatica <Martijn.vanderBruggen@han.nl>
2018-12-18 21:46:17 +0100

    GUIupdate

---
+++ week7GUI/complex/2_textWidget.py
@@ -0,0 +1,22 @@
+from tkinter import filedialog
+import tkinter
+
+class MyGUI:
+    def __init__(self):
+        self.main_window = tkinter.Tk()
+        self.frame = tkinter.Frame(self.main_window)
+        self.my_button = tkinter.Button(self.main_window, text='klik!',\
+                                        command=self.open_bestand)
+        self.my_button.pack()
+        self.invoer = tkinter.Entry (self.main_window,width=10)
+        self.invoer.pack()
+        self.frame.pack()
+        tkinter.mainloop()
+
+
+    def open_bestand(self):
+        self.main_window.filename = filedialog.askopenfilename(initialdir="/",
+                                                               title="Select file",
+                                                               filetypes=(("All files", "*.*"),
+                                                                          ("Fasta files", "*.fasta")))
+my_gui = MyGUI()
