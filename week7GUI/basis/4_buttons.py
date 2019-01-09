cd54806f36875cc15ee893a85b65c23ab1a722d1
martijn <Martijn.vanderBruggen@han.nl>
2019-01-09 12:46:24 +0100

    GUIbi1ab

--- week7GUI/basis/4_buttons.py
+++ week7GUI/basis/4_buttons.py
@@ -7,6 +7,7 @@ import tkinter
 from tkinter import messagebox
 
 class MyGUI:
+
     def __init__(self):
         self.main_window = tkinter.Tk()
         self.label = tkinter.Label(self.main_window, text='Hello World!')
--- week7GUI/liveDemo/bi1ab.py
+++ week7GUI/liveDemo/bi1ab.py
@@ -4,43 +4,67 @@ Voorbeeld van GUI
 Creatie d.d. 8 januari 2019
 """
 
-import tkinter
+import tkinter as tk
+from tkinter import filedialog #speciaal voor filedialog een import
 
 
 class MyGUI:
 
     def __init__(self): # constructor
-        self.main_window = tkinter.Tk()
-        self.main_window.geometry ("500x500") # bepaal grootte van window
+        self.main_window = tk.Tk()
+        self.main_window.geometry ("500x700") # bepaal grootte van window
         self.main_window.title ("GUI App voor Bi1a en Bi1b")
 
-        self.bovenste_frame = tkinter.Frame(self.main_window,
+        self.bovenste_frame = tk.Frame(self.main_window,
                                             background="darkgray",  # zet de achtergrondkleur van het frame
-                                            width=200,              # in dit geval een hexadecimale codering
-                                            height=200,             # kan ook met de naam van een kleur (zie hier onder)
-                                            borderwidth=2)
+                                            width=400,              # breedte
+                                            height=200,             # hoogte van het frame
+                                            borderwidth=2)          # frame kun je ook opmaak meegeven
 
-        self.onderste_frame = tkinter.Frame(self.main_window,
+        self.onderste_frame = tk.Frame(self.main_window,
                                           background="#c0001c",     # zet de achtergrondkleur van het frame
-                                          width=200,                # in dit geval een hexadecimale codering
+                                          width=400,                # in dit geval een hexadecimale codering
                                           height=200)               # kan ook met de naam van een kleur (zie hier onder)
 
         self.bovenste_frame.pack()                                  # plaats het frame op je window
-        self.bovenste_frame.pack_propagate(0)                       # voorkom dat het frame zich aanpast aan de grootte
-                                                                    # van je objecten
+        self.bovenste_frame.pack_propagate(0)
         self.onderste_frame.pack()
 
         # plaats een label (tekst)
-        self.label1 = tkinter.Label(self.bovenste_frame,            # plaats het in een frame
+        self.label1 = tk.Label(self.bovenste_frame,            # plaats het in een frame
                                     text="Hello World!",            # bepaal de tekst
                                     bg="yellow",                    # geef de background (bg) kleur mee
                                     fg="blue")                      # geef de forground (fg) kleur mee
         self.label1.pack()                                          # plaats het in een je frame op een positie
+        self.my_button = tk.Button(self.bovenste_frame,
+                                   text="Open bestand",
+                                   command=self.open_bestand)
+        self.my_button.pack()
+
+        self.scrollbar = tk.Scrollbar(self.onderste_frame) # (1) maak een scrollbar object
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y) # (2) pack de scrollbar en plaats hem rechts
+
+        self.tekstveld = tk.Text(self.onderste_frame,
+                                 height=20,
+                                 width=60,
+                                 bg="#c0c0c0",
+                                 yscrollcommand=self.scrollbar.set) # (3) plaats scrollbar in tekstveld
+        self.scrollbar.config (command=self.tekstveld.yview) # (4) configuur gedrag
+        self.tekstveld.pack()
 
 
+        tk.mainloop()
 
-        tkinter.mainloop()
+    def open_bestand(self):
+        self.file_name = filedialog.askopenfilename(initialdir="~"
+                                                    ,title="Kies een fasta file"
+                                                    ,filetypes=(("Fasta files","*.fa*"),("Alle bestanden","*.*")))
+        print(self.file_name)
+        bestand = open (self.file_name)
+        for regel in bestand:
+            self.tekstveld.insert(tk.END, regel)
+        bestand.close()
 
 
-my_gui = MyGUI() # instantiering van het MyGUI object
+onzin = MyGUI() # instantiering van het MyGUI object
 
